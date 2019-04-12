f8fbcbdd8d77bc028f47ecff53da45e3fd6946ab
unknown <caylenepicache@gmail.com>
2019-04-11 20:07:16 -0700

    beginning of #2

--- modularjs.js
+++ modularjs.js
@@ -1,5 +1,36 @@
-/* var people = [];
-var template = $('#people-template').html();
+(function() {
+    var people ={
+    people: [],
+    template:$('#people-template').html(),
+    init: function(){
+        this.cacheDom();
+    },
+    cacheDom: function(){
+        this.$el = $('#peopleModule');
+        this.$button = this.$el.find('button');
+        this.$input = this.$el.find('input');
+        this.$ul = this.$el.find('ul');
+        this.$template = this.$el.find('#people-template').html();
+    },
+    render: function(){
+        var data = {
+            people: this.people,
+
+        }
+        $('#peopleModule').find('ul').html(Mustace.render(template,data));
+    }
+};
+
+people.init();
+})()
+
+
+//var people = [];
+//var template = $('#people-template').html();
+
+(function() {
+
+/*
 
 $('#peopleModule').find('button').on('click', function(){
     people.push($('#peopleModule').find('input').val());
@@ -22,10 +53,14 @@ $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
     people.splice(i, 1);
 });
 
- */
+*/
+
+})()
+
+ 
 
  //object literal
-
+/*
  var myModule = {
      name: 'Cay',
      age: 27,
@@ -40,3 +75,5 @@ $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
  myModule.setName('Joy');
 
  myModule.sayName();
+
+ */
